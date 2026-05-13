fa4d7c00a989e57f40ac6b4d1bcfed1efa591425
saurav-solanki-cx <saurav.solanki@crediblex.io>
2026-05-13 11:58:25 +0530

    feat(LMS-68): search button by loan number and invoice number

--- src/app/search/search-page/search-page.component.ts
+++ src/app/search/search-page/search-page.component.ts
@@ -1,9 +1,15 @@
 /** Angular Imports */
-import { Component, ViewChild } from '@angular/core';
-import { MatPaginator } from '@angular/material/paginator';
-import { MatTableDataSource } from '@angular/material/table';
-import { Router, ActivatedRoute } from '@angular/router';
-import { SearchData } from '../search.model';
+import { Component } from '@angular/core';
+import { UntypedFormControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+
+/** rxjs Imports */
+import { finalize } from 'rxjs/operators';
+
+/** Custom Services */
+import { LoanRecordSearchResult, SearchService } from '../search.service';
+
+type SearchType = 'loanId' | 'invoiceNo';
 
 /**
  * Search Page Component
@@ -14,120 +20,78 @@ import { SearchData } from '../search.model';
   styleUrls: ['./search-page.component.scss']
 })
 export class SearchPageComponent {
-  /** Flags if number of search results exceed 200 */
-  overload: boolean;
-  /** Datasource for loans disbursal table */
-  dataSource: MatTableDataSource<SearchData>;
-  /** Displayed Columns for serach results */
-  displayedColumns: string[] = [
-    'entityType',
-    'entityName',
-    'entityAccount',
-    'externalId',
-    'parentType',
-    'parentName',
-    'details'
+  searchType = new UntypedFormControl('loanId');
+  searchValue = new UntypedFormControl('');
+  searchTypeOptions = [
+    { value: 'loanId', label: 'Loan ID' },
+    { value: 'invoiceNo', label: 'Invoice Number' }
   ];
-  /** Paginator for the table */
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
-
-  hasResults = false;
+  results: LoanRecordSearchResult[] = [];
+  searchedValue = '';
+  submitted = false;
+  loading = false;
 
   /**
    * @param {ActivatedRoute} route Activated Route
-   * @param {Router} router Router
+   * @param {SearchService} searchService Search Service
    */
   constructor(
     private route: ActivatedRoute,
-    private router: Router
+    private searchService: SearchService
   ) {
-    this.route.data.subscribe((data: { searchResults: any }) => {
-      this.dataSource = new MatTableDataSource(data.searchResults);
-      this.dataSource.paginator = this.paginator;
-      this.hasResults = data.searchResults.length > 0;
-      this.overload = data.searchResults.length > 200 ? true : false;
-      if (this.overload) {
-        this.dataSource = new MatTableDataSource(data.searchResults.slice(0, 200));
+    this.route.queryParamMap.subscribe((params) => {
+      const type = params.get('type') as SearchType;
+      const value = params.get('value');
+      if ((type === 'loanId' || type === 'invoiceNo') && value) {
+        this.searchType.patchValue(type, { emitEvent: false });
+        this.searchValue.patchValue(value, { emitEvent: false });
+        this.search();
       }
     });
   }
 
-  /**
-   * Returns link to entity view page.
-   * @param {any} entity Entity
-   */
-  navigate(entity: SearchData) {
-    switch (entity.entityType) {
-      case 'CLIENT':
-        this.router.navigate([
-          'clients',
-          entity.entityId,
-          'general'
-        ]);
-        break;
-      case 'CLIENTIDENTIFIER':
-        this.router.navigate([
-          'clients',
-          entity.parentId,
-          'general'
-        ]);
-        break;
-      case 'CENTER':
-        this.router.navigate([
-          'centers',
-          entity.entityId
-        ]);
-        break;
-      case 'GROUP':
-        this.router.navigate([
-          'groups',
-          entity.entityId
-        ]);
-        break;
-      case 'SHARE':
-        this.router.navigate([
-          'clients',
-          entity.parentId,
-          'shares-accounts',
-          entity.entityId
-        ]);
-        break;
-      case 'SAVING':
-        if (entity.subEntityType === 'depositAccountType.recurringDeposit') {
-          this.router.navigate([
-            'clients',
-            entity.parentId,
-            'recurring-deposits-accounts',
-            entity.entityId,
-            'transactions'
-          ]);
-        } else if (entity.subEntityType === 'depositAccountType.fixedDeposit') {
-          this.router.navigate([
-            'clients',
-            entity.parentId,
-            'fixed-deposits-accounts',
-            entity.entityId,
-            'transactions'
-          ]);
-        } else if (entity.subEntityType === 'depositAccountType.savingsDeposit') {
-          this.router.navigate([
-            'clients',
-            entity.parentId,
-            'savings-accounts',
-            entity.entityId,
-            'transactions'
-          ]);
-        }
-        break;
-      case 'LOAN':
-        this.router.navigate([
-          'clients',
-          entity.parentId,
-          'loans-accounts',
-          entity.entityId,
-          'general'
-        ]);
-        break;
+  get inputLabel(): string {
+    return this.searchType.value === 'invoiceNo' ? 'Enter Invoice Number' : 'Enter Loan ID';
+  }
+
+  search(): void {
+    const value = String(this.searchValue.value || '').trim();
+    if (!value || this.loading) {
+      return;
     }
+    this.submitted = true;
+    this.searchedValue = value;
+    this.results = [];
+    this.loading = true;
+
+    this.searchService
+      .searchLoanRecords(this.searchType.value, value)
+      .pipe(finalize(() => (this.loading = false)))
+      .subscribe(
+        (results: LoanRecordSearchResult[]) => {
+          this.results = this.searchType.value === 'loanId' ? results.slice(0, 1) : results;
+        },
+        () => {
+          this.results = [];
+        }
+      );
+  }
+
+  loanLink(result: LoanRecordSearchResult): any[] {
+    const parentSegment = result.clientId ? 'clients' : 'groups';
+    const parentId = result.clientId || result.groupId;
+    const tab = this.searchType.value === 'invoiceNo' ? 'loc-details' : 'general';
+    return [
+      '/',
+      parentSegment,
+      parentId,
+      'loans-accounts',
+      result.loanId,
+      tab
+    ];
+  }
+
+  queryParams(result: LoanRecordSearchResult): any {
+    return this.searchType.value === 'invoiceNo' ? { invoiceNo: result.invoiceNo } : {};
   }
 }
